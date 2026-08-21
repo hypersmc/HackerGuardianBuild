d49ed50324a6b2bceb89e28c8f3c40c0ec4a7a01
JumpWatch <mhypers@gmail.com>
2026-08-21 22:36:50 +0200

    Fix legacy AI learning access

--- src/main/java/me/hackerguardian/main/aicore/aievents/HGPlayerInteractListener.java
+++ src/main/java/me/hackerguardian/main/aicore/aievents/HGPlayerInteractListener.java
@@ -1,14 +1,9 @@
 package me.hackerguardian.main.aicore.aievents;
 
 import me.hackerguardian.main.HackerGuardian;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.util.Vector;
-import org.neuroph.core.data.DataSet;
 
 /**
  * @author JumpWatch on 07-04-2023
@@ -20,7 +15,7 @@ public class HGPlayerInteractListener implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (main.learning) {
+        if (main.isLearning()) {
         }
     }
 }
